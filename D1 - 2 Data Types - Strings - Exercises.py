9880677781c987f7c9a0ed3ed8e85a47de6fbd49
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-12-03 20:57:42 -0600

    Python Practice

---
+++ D1 - 2 Data Types - Strings - Exercises.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  7 01:06:05 2019
+
+@author: SHAIL
+"""
+
+
+#exercises =================================================================
+
+s = "Hey there! what should this string be?"
+# find length of the above string
+print(len(s))
+
+# First occurrence of "s"
+print(s[16])
+
+# Number of a's should be ?
+print(s.count("a"))
+
+# Slicing the string into bits
+
+word = s.split(" ")
+print(word)
+
+#1 - The first five characters are 
+print(s[0:6])
+
+#2 - The next five characters are 
+print(s[6:])
+#3 - The thirteenth character is 
+print(s[14])
+
+#4 - length of total string
+print()
+
+#5 -  Convert everything to uppercase
+print()
+
+
+# Convert everything to lowercase
+print()
+
+
+# Split the string into separate strings,
+# each containing only a word
+print()
+
+
+s ="  tobacco  free  workplace  "
+
+"tobacco free workplace"
+
+b = 10.789
+type(b)
+
+b=str(b)
+
+
+
+
+
