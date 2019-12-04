a4afe86f2fe00dc8e91da1055b7379ef2e563bb7
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-12-03 21:39:02 -0600

    Python Practice

--- PasswordChallenge.py
+++ PasswordChallenge.py
@@ -6,3 +6,57 @@ Created on Tue Dec  3 20:55:50 2019
 @author: christophercatzin
 """
 
+# ============================= PASSWORD VALIDATION ==========================
+
+def password_chk(password): 
+      
+    SpecialSym =['$', '@', '#', '%','!'] 
+    value = True
+      
+    # SHOULD HAVE THE LENGTH AT LEAST 5
+    if len(password) < 5: 
+        print('Length should be at least 5') 
+        value = False
+          
+    # SHOULD NOT BE GREATER THAN 9    
+    if len(password) > 9: 
+        print('length should be not be greater than 9') 
+        value = False
+          
+    # GETS THE CHARACTERS FROM THE PASSWORD
+    # AND CHECKS IF IT HAS AT LEASE 1 NUMBER
+    if not any(char.isdigit() for char in password): 
+        print('Password should have at least one numeral') 
+        value = False
+    
+    # CHECKS IF THE PASSWORD HAS AT LEAST ONE UPPERCASE      
+    if not any(char.isupper() for char in password): 
+        print('Password should have at least one uppercase letter') 
+        value = False
+          
+    # CHECKS IF THE PASSWORD HAS AT LEAST ONE LOWER CASE
+    if not any(char.islower() for char in password): 
+        print('Password should have at least one lowercase letter') 
+        value = False
+    
+    # CHECKS IF THE PASSWORD HAS ONE OF THE SPECIAL CHARACTERS      
+    if not any(char in SpecialSym for char in password): 
+        print('Password should have at least one of the symbols $@#') 
+        value = False
+    if value: 
+        return value
+  
+# ================================ MAIN METHOD! =============================
+        
+# THIS GETS THE INPUT PASSWORD, AND VALIDATES IT FROM THE password_chk function
+def main(): 
+    password = 'Chr!zk@t1'
+      
+    if (password_chk(password)): 
+        print("Password is valid") 
+    else: 
+        print("Invalid Password !!") 
+          
+# =============================== DRIVE CODE =================================       
+if __name__ == '__main__': 
+    main()  
